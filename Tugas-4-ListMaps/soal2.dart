c5434a4a131c3cf245840295f4fa3a722279af54
NanaCasmana <awnana123@gmail.com>
2023-11-10 09:19:38 +0700

    add task4

---
+++ Tugas-4-ListMaps/soal2.dart
@@ -0,0 +1,20 @@
+List<int> rangeWithStep(int startNum, int finishNum, int step) {
+  List<int> result = [];
+  if (startNum <= finishNum) {
+    for (int i = startNum; i <= finishNum; i += step) {
+      result.add(i);
+    }
+  } else {
+    for (int i = startNum; i >= finishNum; i -= step) {
+      result.add(i);
+    }
+  }
+  return result;
+}
+
+void main() {
+  print(rangeWithStep(1, 10, 1)); // Untuk rangeWithStep(1, 10, 1)
+  print(rangeWithStep(1, 10, 2)); // Untuk rangeWithStep(1, 10, 2)
+  print(rangeWithStep(11, 23, 3)); // Untuk rangeWithStep(11, 23, 3)
+  print(rangeWithStep(5, 2, 1)); // Untuk rangeWithStep(5, 4, 3, 2)
+}
